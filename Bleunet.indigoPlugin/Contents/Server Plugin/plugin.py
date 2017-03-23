d0b118d5e6a848c3e9e4357edb97b2741d87786a
tperfitt <tperfitt@twocanoes.com>
2017-03-22 21:39:44 -0500

    removed MenuItems since it wasn't being used added single button and motion relay prevent serial from blocking so stopping is faster

--- Bleunet.indigoPlugin/Contents/Server Plugin/plugin.py
+++ Bleunet.indigoPlugin/Contents/Server Plugin/plugin.py
@@ -26,7 +26,7 @@ class Plugin(indigo.PluginBase):
 	########################################
 	def __init__(self, pluginId, pluginDisplayName, pluginVersion, pluginPrefs):
 		super(Plugin, self).__init__(pluginId, pluginDisplayName, pluginVersion, pluginPrefs)
-		self.debug = True
+		self.debug = False
 		self.triggers=[]
 		self.last=0
 	def __del__(self):
@@ -55,67 +55,73 @@ class Plugin(indigo.PluginBase):
 
 
 	def runConcurrentThread(self):
-		self.debugLog(self.pluginPrefs)
 		serial = Serial(self.pluginPrefs['devicePortFieldId_serialPortLocal'], timeout=1)		
 		sequences = dict()
-		trig=0
 		try:
 			while True:
 			# Do your stuff here
-				data = serial.readline().translate(None, b'\x00')
-				if data:
-					self.debugLog("read data from serial: %s" % data.decode('ascii'))
-					try:
-						obj = json.loads(data.decode('ascii'))
-						id = obj['id']
-						seq = int(obj['seq'], 16)
-						state = int(obj['state'])
-						self.last = sequences.get(id) or 0
-					except KeyError:
-						self.debugLog('Error retrieving info from JSON object')
-						seq = 0
-					except ValueError:
-						self.debugLog('Error decoding JSON object')
-						obj = {}
-						seq = 0
-						self.last = 0
-					if seq > self.last:
-						sequences[id] = seq
-						for y in indigo.devices:
-							if "NodeID" in y.pluginProps:
-								self.debugLog(y.pluginProps["NodeID"])
-								nodeID=y.pluginProps["NodeID"]
-								if nodeID.lower()==id.lower():
-									if (state == 01):
-										y.updateStateOnServer('buttonOnePressed', value=True)
-										y.updateStateOnServer('buttonOnePressed', value=False)
-									elif (state == 11):
-										y.updateStateOnServer('buttonTwoPressed', value=True)
-										y.updateStateOnServer('buttonTwoPressed', value=False)
-									elif (state == 21):
-										y.updateStateOnServer('buttonThreePressed', value=True)
-										y.updateStateOnServer('buttonThreePressed', value=False)
-									elif (state == 31):
-										y.updateStateOnServer('buttonFourPressed', value=True)
-										y.updateStateOnServer('buttonFourPressed', value=False)
-									if (state == 02):
-										y.updateStateOnServer('buttonOneLongPress', value=True)
-										y.updateStateOnServer('buttonOneLongPress', value=False)
-									elif (state == 12):
-										y.updateStateOnServer('buttonTwoLongPress', value=True)
-										y.updateStateOnServer('buttonTwoLongPress', value=False)
-									elif (state == 22):
-										y.updateStateOnServer('buttonThreeLongPress', value=True)
-										y.updateStateOnServer('buttonThreeLongPress', value=False)
-									elif (state == 32):
-										y.updateStateOnServer('buttonFourLongPress', value=True)
-										y.updateStateOnServer('buttonFourLongPress', value=False)
+				if (serial.inWaiting()>0):
+					data = serial.readline().translate(None, b'\x00')
+					if data:
+						self.debugLog("read data from serial: %s" % data.decode('ascii'))
+						try:
+							obj = json.loads(data.decode('ascii'))
+							id = obj['id']
+							seq = int(obj['seq'], 16)
+							state = int(obj['state'])
+							deviceClass=int(obj['class'])
+							self.last = sequences.get(id) or 0
+						except KeyError:
+							self.debugLog('Error retrieving info from JSON object')
+							seq = 0
+						except ValueError:
+							self.debugLog('Error decoding JSON object')
+							obj = {}
+							seq = 0
+							self.last = 0
+						if seq > self.last:
+							sequences[id] = seq
+							for y in indigo.devices.iter("self"):
+								if "NodeID" in y.pluginProps:
+									self.debugLog(y.pluginProps["NodeID"])
+									nodeID=y.pluginProps["NodeID"]
+									if nodeID.lower()==id.lower():
+										if (deviceClass == 8):
+											if (state == 01):
+												y.updateStateOnServer('buttonOnePressed', value=True)
+												y.updateStateOnServer('buttonOnePressed', value=False)
+											elif (state == 11):
+												y.updateStateOnServer('buttonTwoPressed', value=True)
+												y.updateStateOnServer('buttonTwoPressed', value=False)
+											elif (state == 21):
+												y.updateStateOnServer('buttonThreePressed', value=True)
+												y.updateStateOnServer('buttonThreePressed', value=False)
+											elif (state == 31):
+												y.updateStateOnServer('buttonFourPressed', value=True)
+												y.updateStateOnServer('buttonFourPressed', value=False)
+											if (state == 02):
+												y.updateStateOnServer('buttonOneLongPress', value=True)
+												y.updateStateOnServer('buttonOneLongPress', value=False)
+											elif (state == 12):
+												y.updateStateOnServer('buttonTwoLongPress', value=True)
+												y.updateStateOnServer('buttonTwoLongPress', value=False)
+											elif (state == 22):
+												y.updateStateOnServer('buttonThreeLongPress', value=True)
+												y.updateStateOnServer('buttonThreeLongPress', value=False)
+											elif (state == 32):
+												y.updateStateOnServer('buttonFourLongPress', value=True)
+												y.updateStateOnServer('buttonFourLongPress', value=False)
+										if (deviceClass == 9):  #motion sensor
+											if (state == 0):
+												y.updateStateOnServer('motionDetected', value=False)
+											elif (state == 1):
+												y.updateStateOnServer('motionDetected', value=True)
 
-																			
+																				
 
 						
 
-# 				self.sleep(3) # in seconds
+ 				self.sleep(.25) # in seconds
 		except self.StopThread:
 			# do any cleanup here
 			pass		
