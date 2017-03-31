4b030ecc3e0da21937c0dc21a961981ce9dfa4b7
Dave Lebbing <dlebbing@twocanoes.com>
2017-03-31 15:21:47 -0500

    Refactor beacon detector absence filter to work with new firmware
    Filter on time instead of repeated absence reports

--- Bleunet.indigoPlugin/Contents/Server Plugin/plugin.py
+++ Bleunet.indigoPlugin/Contents/Server Plugin/plugin.py
@@ -29,11 +29,9 @@ class Plugin(indigo.PluginBase):
 		self.debug = False
 		self.triggers=[]
 		self.last=0
-		self.last_asset=""
 		self.beacon_range = range(0,11)
-		self.beacon_absence = {}
-		for beacon in self.beacon_range:
-			self.beacon_absence[beacon] = 0
+		self.beacon_last_seen = {}
+		self.beacon_absence_filter = 40 # seconds
 	def __del__(self):
 		indigo.PluginBase.__del__(self)
 	######################
@@ -60,6 +58,8 @@ class Plugin(indigo.PluginBase):
 	def loadPluginPrefs(self):
 		self.debugLog(u"loadpluginPrefs called")	
 		self.debug = self.pluginPrefs.get('debugEnabled',False)
+		self.beacon_absence_filter = format_beacon_absence_filter(self.pluginPrefs.get('beaconAbsenceFilter', '0'))
+		self.debugLog("Beacon absence filter: {} seconds".format(self.beacon_absence_filter))
 
 	def closedPrefsConfigUi ( self, valuesDict, UserCancelled):
 		if UserCancelled is False:
@@ -78,27 +78,22 @@ class Plugin(indigo.PluginBase):
 						self.debugLog("read data from serial: %s" % data.decode('ascii'))
 						try:
 							obj = json.loads(data.decode('ascii'))
-							id = obj.get("id","0")
-							seq = int(obj.get("seq","00"),16)
-							state = int(obj.get("state","00"),16)
-							deviceClass=int(obj.get("class",""),16)
-							asset=obj.get("assetfield","")
-							group=int(obj.get("grpnum","FF"), 16)
-							self.last = sequences.get(id,0)
-						except KeyError as e:
-							self.debugLog('Error retrieving info from JSON object'+ e.errno)
-							seq = 0
 						except ValueError:
 							self.debugLog('Error decoding JSON object')
 							obj = {}
-							seq = 0
-							self.last = 0
+						device_id = obj.get("id","0")
+						seq = int(obj.get("seq","00"),16)
+						state = int(obj.get("state","00"),16)
+						deviceClass=int(obj.get("class","00"),16)
+						asset=obj.get("assetfield","")
+						group=int(obj.get("grpnum","FF"), 16)
+						self.last = sequences.get(device_id,0)
 						if seq > self.last:
-							sequences[id] = seq
+							sequences[device_id] = seq
 							for y in indigo.devices.iter("self"):
 								if "NodeID" in y.pluginProps:
 									nodeID=y.pluginProps["NodeID"]
-									if nodeID.lower()==id.lower():
+									if nodeID.lower() == device_id.lower():
 										if (deviceClass == 0x8):  #button
 											state_options = {'Pressed': {'One': 0x01, 'Two': 0x11, 'Three': 0x21, 'Four': 0x31}, 
 												'LongPress': {'One': 0x02, 'Two': 0x12, 'Three': 0x22, 'Four': 0x32}}
@@ -115,31 +110,20 @@ class Plugin(indigo.PluginBase):
 												y.updateStateOnServer('motionDetected', value=True)
 										if (deviceClass == 0x0a):  #beacon detector
 											if group == 0x00:
-												if asset != self.last_asset:
-													self.last_asset = asset
-													asset_int = int(asset, 16)
-													for beacon_number in self.beacon_range:
-														if bit_present(asset_int, beacon_number):
-															self.beacon_absence[beacon_number] = 0
-															if y.states.get("beaconNumber{}".format(beacon_number)) != True:
-																indigo.server.log("found beacon {}".format(beacon_number))
-																y.updateStateOnServer("beaconNumber{}".format(beacon_number), value=True)
-															else:
-																self.debugLog("beacon {} detected".format(beacon_number))
-														else:
-															self.debugLog("beacon {} not detected".format(beacon_number))
-															self.beacon_absence[beacon_number] += 1
-												else:
-													beacon_absence_filter = int(y.pluginProps.get("absenceFilter", 3))
-													self.debugLog("Absence filter: {}".format(beacon_absence_filter))
-													for beacon_number in self.beacon_absence.keys():
-														if self.beacon_absence[beacon_number] > 0:
-															if self.beacon_absence[beacon_number] < beacon_absence_filter:
-																self.beacon_absence[beacon_number] += 1
-															elif self.beacon_absence[beacon_number] >= beacon_absence_filter:
-																if y.states.get("beaconNumber{}".format(beacon_number)) != False:
-																	indigo.server.log("lost beacon {}".format(beacon_number))
-																	y.updateStateOnServer("beaconNumber{}".format(beacon_number), value=False)
+												asset_int = int(asset, 16)
+												for beacon_number in self.beacon_range:
+													beacon_previous_state = y.states.get("beaconNumber{}".format(beacon_number))
+													if bit_present(asset_int, beacon_number):
+														self.debugLog("beacon {} detected".format(beacon_number))
+														self.beacon_last_seen[beacon_number] = time.time()
+														if beacon_previous_state != True:
+															indigo.server.log("found beacon {}".format(beacon_number))
+															y.updateStateOnServer("beaconNumber{}".format(beacon_number), value=True)
+													else:
+														seconds_since_seen = time.time() - self.beacon_last_seen.get(beacon_number, 0)
+														if (seconds_since_seen > self.beacon_absence_filter) and (beacon_previous_state != False):
+															indigo.server.log("lost beacon {}".format(beacon_number))
+															y.updateStateOnServer("beaconNumber{}".format(beacon_number), value=False)
  				self.sleep(.25) # in seconds
 		except self.StopThread:
 			# do any cleanup here
@@ -149,3 +133,7 @@ def bit_present(number, bit_position):
 	bitmask = 2 ** bit_position
 	return number & bitmask == bitmask
 
+def format_beacon_absence_filter(value):
+	if (not value.isdigit()) or (int(value) < 0):
+		value = 0
+	return int(value)
